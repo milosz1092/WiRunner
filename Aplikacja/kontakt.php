11243fb49aa6f45514f5eacc38959845f21807d7
Miłosz Szewczyk <milosz1092@gmail.com>
2014-04-08 09:41:34 +0200

    Aplikacja
    v1.0

--- Aplikacja/kontakt.php
+++ Aplikacja/kontakt.php
@@ -1,7 +1,19 @@
 <?php
 	include('php/top.php');
 ?>
+		<article>
+			<section>
+				<div class="left_part"  style="width:500px;margin-right:70px;">
+					<header class="entry-header hr_bor">
+						<h1 class="entry-title">Kontakt</h1>
+					</header>
 
+				</div>
+				<div class="right_part">
+				
+				</div>
+			</section>
+		</article>
 <?php
 	include('php/bottom.php');
 ?>
