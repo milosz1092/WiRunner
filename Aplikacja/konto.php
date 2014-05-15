9b9006d7b8b01e86b332c65e993a1ca90c9121ff
Łukasz Staniszczak <prymus007@gmail.com>
2014-05-15 18:27:01 +0200

    Wyszukiwarka.php + konto.php

--- Aplikacja/konto.php
+++ Aplikacja/konto.php
@@ -13,9 +13,6 @@
 if(!$my_userAction->get_coordinates(1))
 	echo '<a href="./wspolrzedne.php">Ustaw swoje współrzędne na mapie!</a><br/>';
 
-<<<<<<< HEAD
-
-=======
 /*
 $przyjaciele = $my_usersRelations->znajdz_userow_w_relacji($_SESSION['WiRunner_log_id'], "Przyjaciel");
 	foreach($przyjaciele as $ele){
@@ -23,7 +20,6 @@ $przyjaciele = $my_usersRelations->znajdz_userow_w_relacji($_SESSION['WiRunner_l
 
 
 }*/
->>>>>>> 4732a2f8092608801585e60222e9b6f1ca9c5e2e
 ?>
 <div id="big_contener">
 	<div id="left_contener">
@@ -71,13 +67,8 @@ $przyjaciele = $my_usersRelations->znajdz_userow_w_relacji($_SESSION['WiRunner_l
 						if($my_userAction->profile_update($dane) == -1){
 							$my_userAction->profil_edit($dane);
 							break;
-<<<<<<< HEAD
-						} else
-							echo '<div class="ok_msg">Pomyślnie zaktualizowano dane!</div>';
-=======
 
 						} else echo '<div class="ok_msg">Pomyślnie zaktualizowano dane!</div>';
->>>>>>> 4732a2f8092608801585e60222e9b6f1ca9c5e2e
 					}
 
 					$userInfo = $my_simpleDbCheck->getUserInfo($_SESSION['WiRunner_log_id']);
