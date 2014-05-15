656511ac4cb397fd1b3e8891ffb3df18a918738e
Łukasz Staniszczak <prymus007@gmail.com>
2014-05-15 20:57:48 +0200

    Drobne
    jeszcze jedna zaległa poprawka + trochę nowości

--- Aplikacja/php/classes/my_usersRelations.php
+++ Aplikacja/php/classes/my_usersRelations.php
@@ -74,7 +74,40 @@
 	
 		function ustaw_relacje($id_userow, $relacja)
 		{
-
+			$id_userow['1st'] > $id_userow['2nd'] ? list($id_userow['1st'],$id_userow['2nd']) = array($id_userow['2nd'], $id_userow['1st']) : "";
+			try {
+$stmt = $this -> pdo -> prepare('SELECT id_relacji FROM rodzaje_relacji WHERE relacja LIKE BINARY :relacja');
+				$stmt -> bindValue(':relacja', $id_userow['1st'], PDO::PARAM_STR);
+				$stmt -> execute();
+				if($stmt -> rowCount() != 1) {
+					 return -1;
+				}
+				else {
+					$row = $stmt -> fetch();
+					$rodzaj = $row['id_relacji'];
+				}
+				$stmt -> closeCursor();
+				unset($stmt);
+		
+				$stmt = $this -> pdo -> prepare('INSERT INTO relacje_uzytkownikow VALUES (:nr_pierwszego,:nr_drugiego,:rodzaj,:data) ON DUPLICATE KEY UPDATE nr_rodzaju=:rodzaj, data=:data');
+				$stmt -> bindValue(':nr_pierwszego', $id_userow['1st'], PDO::PARAM_INT);
+				$stmt -> bindValue(':nr_drugiego', $id_userow['2nd'], PDO::PARAM_STR);
+				$stmt -> bindValue(':rodzaj', $relacja, PDO::PARAM_STR);
+				$stmt -> bindValue(':data', date("Y-m-d H:i:s"), PDO::PARAM_STR);
+				$stmt -> execute();
+				if($stmt -> rowCount() != 1) {
+					// return 0;
+				}
+				else {
+					$row = $stmt -> fetch();
+				}
+				$stmt -> closeCursor();
+				unset($stmt);
+			}
+			catch(PDOException $e) {
+				//echo '<p>Wystąpił błąd biblioteki PDO</p>';
+				return 0;
+			}
 		}
 
 	}
