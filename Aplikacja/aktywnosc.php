40befe3a452bfdba31267590788ef39e55a3d555
Łukasz Staniszczak <prymus007@gmail.com>
2014-05-29 16:52:21 +0200

    Wyświetlanie mapy w szczegółach aktywności
    + trochę więcej

--- Aplikacja/aktywnosc.php
+++ Aplikacja/aktywnosc.php
@@ -1,11 +1,18 @@
 <?php
 	include('php/top.php');
 
-	if(empty($_GET['id']) || ($dane=$my_activities->getActivityById($_GET['id'])) == 0)
+	if(empty($_GET['id']) || !intval($_GET['id']) || ($dane=$my_activities->getActivityById($_GET['id'])) == 0)
 		header("Location: szukaj.php");
 
 	if($_SESSION['WiRunner_log_id'] == 0 && $dane['widoczna_dla_gosci'] == 0)
 		header("Location: login.php");
+
+	$dane = $my_activities->getActivityById($_GET['id']);
+
+	if($_SESSION['WiRunner_log_id'] != 0 && $_SESSION['WiRunner_log_id'] != $dane['nr_uzytkownika'] && in_array($my_usersRelations->zwroc_typ(
+			array("1st" => $_SESSION['WiRunner_log_id'],
+			      "2nd" => $dane['nr_uzytkownika'])), array("Wróg", "Blokowany")))
+		header("Location: index.php");
 
 	if(isset($_POST['dodajKomentarz'])){
 
@@ -18,9 +25,85 @@
 	}
 
 
+$user_info = $my_activities->getUserInfo($dane['nr_uzytkownika']);
+$czas = (3600 * $dane['dystans']) / $dane['tempo'];
+
+	echo '<div class="aktywnoscSzczegoly">
+		<img style="display:inline-block;float:left;margin-right:10px;" width="20" height="20" src="img/web/unknow.jpg" alt="avatar" />
+
+<a href="profil.php?uid='.$dane['nr_uzytkownika'].'"><b>'.(isset($user_info['imie'])?$user_info['imie'] . ' ' . $user_info['nazwisko'] : $user_info['email']) . '</b></a> uprawiał <u>' . $my_activities->getSport($dane['nr_sportu']) . '</u>. ';
+			
+	echo 'Przebył '. $dane['dystans'] . 'km w ' . $my_activities->formatujCzas($czas) .'.<br/><br/>';
+	echo 'Data treningu: '.$dane['data_treningu'].'<br/>
+		Nazwa treningu: <b>'.$dane['nazwa_treningu'].'</b><br/>
+		Opis: '.$dane['opis'].'<br/><br/>
+		Data dodania: '.$dane['data_dodania'].'</div>';
+	if($dane['nr_trasy'])
+	{
 	
-	$my_activities->printActivity($_GET['id']);
+	$dane_trasy = $my_userAction->get_track($dane['nr_trasy']);
+	echo '<div id="obszar_mapy" class="float: right">';
+	$wsp = explode("),", $dane_trasy['punkty_trasy']);
+	echo '<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false&v=3"></script>
+	      <script type="text/javascript">
+		var latlng = new google.maps.LatLng'.substr($dane_trasy['punkty_trasy'], 0, strpos($dane_trasy['punkty_trasy'], ")")+1).';
+		var wspolrzedne =new Array();
+		var dystans = '.($dane_trasy['dlugosc_trasy']*1000).';
+		var i = '.sizeof($wsp).';';
+		foreach($wsp as $key => $punkt)
+			echo 'wspolrzedne['.$key.'] =  new google.maps.LatLng'.$punkt.($key+1 != sizeof($wsp)?')':'').';';
+?>
 	
+var marker;
+var mojeUstawienia;
+var mapa;
+var trasa;
+
+mojeUstawienia = {
+    zoom: 14, //!!zmienione powiekszenie!!										
+    center: latlng, 								
+    mapTypeId: google.maps.MapTypeId.ROADMAP 		
+  };
+  mapa = new google.maps.Map(document.getElementById("obszar_mapy"), mojeUstawienia);
+
+
+
+new google.maps.Polyline({
+path: wspolrzedne,
+strokeColor: '#0000FF',
+strokeOpacity: 1.0,
+strokeWeight: 3,
+map: mapa
+});
+
+var dys = 0;
+var obr;
+if(x != 2)
+for(k=0;k<i;k++)
+{
+	if(k>0)
+	dys += distance(k);
+
+	if(k==0) obr="./img/web/start_marker.png";
+	else if(k == i-1 && x!=1) obr="./img/web/meta_marker.png";
+	else obr="./img/web/red_marker.png";
+
+	marker = new google.maps.Marker(
+	   { 
+		draggable: czyPrzesuwany,
+		icon: obr, 
+		position: wspolrzedne[k],
+		map: mapa,
+		title: "Pkt " + (k+1) + ((k >= 1) ? ". \nOd poprzedniego: " + (distance(k)/1000).toFixed(3) + "km"+"\nOd startu: "+(dys/1000).toFixed(3) + "km":"."),
+		flat: false
+	   });
+
+}alert("ok");
+</script>
+<?
+}
+	echo '</div>';
+
 	if(isset($resDodawania) && is_array($resDodawania))
 	my_simpleMsg::show('Błedy danych!', $resDodawania, 0);
 	echo $my_comments->formularzDodawania("doAktywnosci",$_GET['id']);
